7de9abf476d831e89694268443fdea8c8f734f4c
Nikita Divin <Nikita.Divin@bonava.com>
2022-02-11 21:45:34 +0300

    Добавьте файлы проекта.

---
+++ CreationModelPlugin/CreationModel.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationModelPlugin
+{
+    [Transaction(TransactionMode.Manual)]
+    public class CreationModel : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            // Создание стен по прямоугольнику
+            List<Wall> newWalls = WallsHelper.CreateWallsAlongRectangle(doc, "Уровень 1", "Уровень 2", 10000, 5000);
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ CreationModelPlugin/WallsHelper.cs
@@ -0,0 +1,75 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CreationModelPlugin
+{
+    public static class WallsHelper
+    {
+        public static List<Wall> CreateWallsAlongRectangle(Document doc, string level1Name, string level2Name, int widthInMm, int depthInMm)
+        {
+            List<Wall> walls = new List<Wall>();
+
+            if (widthInMm == 0 || depthInMm == 0)
+            {
+                TaskDialog.Show("Ошибка", "Размеры сишком малы");
+                return walls;
+            }
+
+            List<Level> listLevel = new FilteredElementCollector(doc)
+                .OfClass(typeof(Level))
+                .OfType<Level>()
+                .ToList();
+
+            Level level1 = listLevel
+                .Where(x => x.Name.Equals(level1Name))
+                .FirstOrDefault();
+
+            if (level1 == null)
+            {
+                TaskDialog.Show("Ошибка", "Базового уровня с таким именем не существует");
+                return walls;
+            }
+
+            Level level2 = listLevel
+                .Where(x => x.Name.Equals(level2Name))
+                .FirstOrDefault();
+            
+            if (level2 == null)
+            {
+                TaskDialog.Show("Ошибка", "Верхнего уровня с таким именем не существует");
+                return walls;
+            }
+
+
+
+            double width = UnitUtils.ConvertToInternalUnits(widthInMm, UnitTypeId.Millimeters);
+            double depth = UnitUtils.ConvertToInternalUnits(depthInMm, UnitTypeId.Millimeters);
+            double dx = width / 2;
+            double dy = depth / 2;
+
+            List<XYZ> points = new List<XYZ>();
+            points.Add(new XYZ(-dx, -dy, 0));
+            points.Add(new XYZ(dx, -dy, 0));
+            points.Add(new XYZ(dx, dy, 0));
+            points.Add(new XYZ(-dx, dy, 0));
+            points.Add(new XYZ(-dx, -dy, 0));
+
+            Transaction transaction = new Transaction(doc, "Построение стен");
+            transaction.Start();
+            for (int i = 0; i < 4; i++)
+            {
+                Line line = Line.CreateBound(points[i], points[i + 1]);
+                Wall wall = Wall.Create(doc, line, level1.Id, false);
+                walls.Add(wall);
+                wall.get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE).Set(level2.Id);
+            }
+
+            transaction.Commit();
+
+            return walls;
+        }
+    }
+}
